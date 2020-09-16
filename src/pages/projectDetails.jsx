15458e852f1d3f74d6d583171bed96a0369fa1a0
Felix Lehner <f.lehner@tu-bs.de>
2020-09-16 19:16:21 +0200

    adding project Details

--- src/pages/projectDetails.jsx
+++ src/pages/projectDetails.jsx
@@ -2,19 +2,15 @@ import React from 'react';
 import { Page, Navbar, Block, Row, Link } from 'framework7-react';
 import Dom7 from 'dom7';
 //import { WhatsappShareButton, EmailShareButton } from "react-share";
+import * as qrcode from 'qrcode-generator';
+import { SVESystemInfo, SVEProject } from 'svebaselib';
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      projectId: props.f7route.params.id,
-      project: {
-        id: -1,
-        name: '',
-        owner: -1,
-        context: -1
-      },
+      project: Number(props.f7route.params.id),
       ownerName: '',
       token: "",
       link: ""
@@ -23,12 +19,14 @@ export default class extends React.Component {
   render() {
     return (
       <Page name="projectdetails">
-        <Navbar title={`${this.state.project.name} details`} backLink="Back">
+        <Navbar title={(typeof this.state.project !== "number") ? `${this.state.project.getName()} details` : ""} backLink="Back">
         </Navbar>
 
         <Block strong>
-          <Row style={{display: "flex", justifyContent: "center", alignContent: "center"}}>
-            <img style={{width: "70%", maxHeight: "80vh", maxWidth: "80vh"}} src={this.$f7.apiPath + `/generators/QRGenerator.py?payload=` + ((this.state.token.length == 0) ? "" : encodeURI(this.state.link))}/>
+          <Row 
+            style={{display: "flex", justifyContent: "center", alignContent: "center"}}
+            dangerouslySetInnerHTML={this.makeQRCode()}
+          >
           </Row>
           <Row>
             Gegründet von {this.state.ownerName}
@@ -54,45 +52,36 @@ export default class extends React.Component {
   registerToken() {
     var self = this;
     let newToken = [...Array(30)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
-    this.$f7.request.get(this.$f7.apiPath + "/registerToken.php?method=action&token=" + encodeURI(newToken),
-      "text",
-      function(data, status) {
-        self.state.token = newToken;
-        self.state.link = "https://" + location.hostname + "/?page=register&token="+self.state.token + "&context=" + self.state.project.context;
-        self.setState({token: newToken, link: self.state.link});
-      },
-      function (xhr, status) {
-        if (status == 409)
-          self.registerToken();
-      }
-    );
+    self.setState({
+      token: newToken, 
+      link: "https://" + window.location.hostname + "/" + SVESystemInfo.getAPIRoot() + "/?page=register&token="+self.state.token + "&context=" + self.state.project.getGroup().getID()
+    });
+  }
+
+  makeQRCode() {
+    let qr = qrcode.default(0, 'L');
+    qr.addData(this.state.link);
+    qr.make();
+    return qr.createImgTag();
   }
 
   componentDidMount() {
-    var router = this.$f7router;
     var self = this;
-    var $$ = Dom7;
     this.$f7ready((f7) => {
-      self.registerToken();
-      f7.request.get(f7.apiPath + "/getProject.php?id=" + self.state.projectId, function(data) {
-        var Project = JSON.parse(data);
-        if (Project.splash_img == null || Project.splash_img == "null" || Project.splash_img == "")
-        {
-          Project.splash_img = "InProgress.png";
-        }
+      if(!SVESystemInfo.getSystemStatus().tokenSystem) {
+        f7.dialog.alert("Token-System ist offline! Aktuell können keine Einladungen registriert werden.");
+      }
 
-        self.setState({project: Project});
-
-        f7.request.get(f7.apiPath + "/getUserInfo.php?id=" + self.state.project.owner, function(data) {
-          self.state.ownerName = JSON.parse(data).name;
-          self.setState({ownerName: JSON.parse(data).name});
+      if (typeof self.state.project === "number") {
+        new SVEProject(self.state.project, p => {
+          self.state.project = p;
+          self.setState({project: p});
+          self.registerToken();
+          p.getOwner().then(usr => {
+            self.setState({ownerName: usr.getName()});
+          });
         });
-      },
-      function(xhr, status) {
-        self.$f7.dialog.confirm("Zugriff verweigert! Möchten Sie sich unter einem anderen Namen anmelden?", "Zugriff verweigert", 
-        function() { router.navigate('/login/'); },
-        function() { router.back(); });
-      });
+      }
     });
   }
 }
