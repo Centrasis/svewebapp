cb20907f2f351b5a62078f8a1c7a84674584bc62
Felix Lehner <f.lehner@tu-bs.de>
2020-09-17 11:19:01 +0200

    adding project Details

--- src/pages/contextDetails.jsx
+++ src/pages/contextDetails.jsx
@@ -1,53 +1,40 @@
 import React from 'react';
-import { Page, Navbar, Block, Row } from 'framework7-react';
+import { Page, Navbar, Block, Row, Preloader } from 'framework7-react';
 import Dom7 from 'dom7';
+import InviteField from './InviteField';
+import { SVEGroup } from 'svebaselib';
 
 export default class extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      contextID: props.f7route.params.id,
-      token: ""
+      group: Number(props.f7route.params.id)
     };
   }
   render() {
     return (
       <Page name="contextdetails">
-        <Navbar title={`Gruppen beitreten`} backLink="Back">
+        <Navbar title={`Gruppen beitreten: ${(typeof this.state.group !== "number") ? this.state.group.getName() : ""}`} backLink="Back">
         </Navbar>
 
-        <Block strong>
-        <Row style={{display: "flex", justifyContent: "center", alignContent: "center"}}>
-          <img style={{width: "70%", maxHeight: "80vh", maxWidth: "80vh"}} src={this.$f7.apiPath + `/generators/QRGenerator.py?payload=` + ((this.state.token.length == 0) ? "" : encodeURI("https://" + location.hostname + "/?page=register&token="+this.state.token + "&context=" + this.state.contextID))}/>
-        </Row>
-        </Block>
+        {(typeof this.state.group !== "number") ? 
+          <InviteField 
+            group = {this.state.group}
+          />
+        : <Preloader></Preloader> }
       </Page>
     );
   }
 
-  registerToken() {
-    var self = this;
-    let newToken = [...Array(30)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
-    this.$f7.request.get(this.$f7.apiPath + "/registerToken.php?method=action&token=" + encodeURI(newToken),
-      "text",
-      function(data, status) {
-        self.state.token = newToken;
-        self.setState({token: newToken});
-      },
-      function (xhr, status) {
-        if (status == 409)
-          self.registerToken();
-      }
-    );
-  }
-
   componentDidMount() {
-    var router = this.$f7router;
     var self = this;
-    var $$ = Dom7;
     this.$f7ready((f7) => {
-      self.registerToken();
+      if (typeof self.state.group === "number") {
+        new SVEGroup(self.state.group, this.$f7.data.getUser(), g => {
+          self.setState({group: g});
+        });
+      }
     });
   }
 }
--- src/pages/projectDetails.jsx
+++ src/pages/projectDetails.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Page, Navbar, Block, Row, Link } from 'framework7-react';
-import Dom7 from 'dom7';
-//import { WhatsappShareButton, EmailShareButton } from "react-share";
-import * as qrcode from 'qrcode-generator';
-import { SVESystemInfo, SVEProject } from 'svebaselib';
+import { Page, Navbar, Block } from 'framework7-react';
+import { SVEProject } from 'svebaselib';
+import InviteField from './InviteField';
 
 export default class extends React.Component {
   constructor(props) {
@@ -11,9 +9,7 @@ export default class extends React.Component {
 
     this.state = {
       project: Number(props.f7route.params.id),
-      ownerName: '',
-      token: "",
-      link: ""
+      group: undefined
     };
   }
   render() {
@@ -22,47 +18,15 @@ export default class extends React.Component {
         <Navbar title={(typeof this.state.project !== "number") ? `${this.state.project.getName()} details` : ""} backLink="Back">
         </Navbar>
 
-        <Block strong>
-          <Row 
-            style={{display: "flex", justifyContent: "center", alignContent: "center"}}
-            dangerouslySetInnerHTML={this.makeQRCode()}
-          >
-          </Row>
-          <Row>
-            Gegründet von {this.state.ownerName}
-          </Row>
-        </Block>
-        <Block mediumInset>
-          {/*<Row>
-            <WhatsappShareButton url={this.state.link} title="Einladungs Link schicken"/>
-          </Row>
-          <Row>
-            <EmailShareButton url={this.state.link} title="Einladungs Link schicken"/>
-          </Row>*/}
-          <Row>
-            <p>Oder kopiere diesen Link:</p><br></br>
-            <Link tooltip="Kopiere diesen Link">{this.state.link}</Link>
-          </Row>
-        </Block>
+        {(this.state.group !== undefined) ? 
+          <InviteField 
+            group = {this.state.group}
+            project = {this.state.project}
+          />
+        : <Preloader></Preloader> }
         <Block>&nbsp;</Block>
       </Page>
     );
-  }
-
-  registerToken() {
-    var self = this;
-    let newToken = [...Array(30)].map(i=>(~~(Math.random()*36)).toString(36)).join('');
-    self.setState({
-      token: newToken, 
-      link: "https://" + window.location.hostname + "/" + SVESystemInfo.getAPIRoot() + "/?page=register&token="+self.state.token + "&context=" + self.state.project.getGroup().getID()
-    });
-  }
-
-  makeQRCode() {
-    let qr = qrcode.default(0, 'L');
-    qr.addData(this.state.link);
-    qr.make();
-    return {__html: qr.createImgTag()};
   }
 
   componentDidMount() {
@@ -73,12 +37,10 @@ export default class extends React.Component {
       }
 
       if (typeof self.state.project === "number") {
-        new SVEProject(self.state.project, p => {
-          self.state.project = p;
-          self.setState({project: p});
-          self.registerToken();
-          p.getOwner().then(usr => {
-            self.setState({ownerName: usr.getName()});
+        new SVEProject(self.state.project, this.$f7.data.getUser(), p => {
+          self.setState({
+            project: p,
+            group: p.getGroup()
           });
         });
       }
