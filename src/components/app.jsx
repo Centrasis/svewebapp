521c3bc9f9c29e12182405ec5a6579221ca1dc19
Felix Lehner <f.lehner@tu-bs.de>
2020-09-24 17:27:10 +0200

    error screen added on top level

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -94,10 +94,9 @@ export default class extends React.Component {
                   let constraints = {
                     audio: false,
                     video: ((app.$f7.device.android || app.$f7.device.ios) ? {
-                      width: { min: 1280, ideal: 4096, max: 4096 },
-                      height: { min: 720, ideal: 2160, max: 2160 },
-                      facingMode: { exact: (facingUser === true) ? "user" : "environment" },
-                      deviceId: { exact: (facingUser === true) ? "user" : "environment" }
+//                      width: { min: 1280, ideal: 4096, max: 4096 },
+//                      height: { min: 720, ideal: 2160, max: 2160 },
+                      facingMode: (facingUser === true) ? { exact: "user" } : "environment"
                     } : true)
                   };
                   console.log("Request camera stream: " + JSON.stringify(constraints));
