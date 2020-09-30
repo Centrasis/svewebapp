3d530b3a07cef1057d834f5860ce47336d05f781
Felix Lehner <f.lehner@tu-bs.de>
2020-09-30 12:37:50 +0200

    adding timeline to groups

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -244,7 +244,7 @@ export default class extends React.Component {
               <NavTitle sliding>Ein kritischer Fehler trat auf!</NavTitle>
               <NavTitleLarge sliding>Ein kritischer Fehler trat auf!</NavTitleLarge>
               <NavRight>
-                <Link external iconF7="text_bubble" tooltip="Fehler melden" href={"mailto:info@felixlehner.de?subject=Webseitenfehler&body=Fehler%20trat%20auf%3A%0D%0A" + this.state.error.msg} />
+                <Link external iconF7="text_bubble" tooltip="Fehler melden" href={"mailto:info@felixlehner.de?subject=Webseitenfehler&body=Fehler%20trat%20auf%3A%0D%0A" + this.state.error.msg.replace("\\n", "\n")} />
                 <Link style={{color: "green"}} iconF7="tornado" tooltip="Fehler auflösen" onClick={() => window.location.reload()} />
               </NavRight>
             </Navbar>
