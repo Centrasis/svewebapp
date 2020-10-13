dc946f8845a94376a3a80982428427d5a80d5c82
Felix Lehner <f.lehner@tu-bs.de>
2020-10-13 22:32:40 +0200

    adding tensorflow support in documents

--- src/pages/docs.jsx
+++ src/pages/docs.jsx
@@ -99,7 +99,7 @@ export default class extends React.Component {
   predict(model, videoElem) {
     console.log("Got frame from camera..");
     let tensor = tf.browser.fromPixels(videoElem);
-    tensor = tf.image.resize(tensor, [224, 224]);
+    tensor = tf.image.resizeBilinear(tensor, [224, 224]);
     const eTensor = tensor.expandDims(0).asType('float32').div(256.0);
     const prediction = model.predict(eTensor);
     console.log("Prediction: " + JSON.stringify(prediction));
